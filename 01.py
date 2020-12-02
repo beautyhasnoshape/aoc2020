280401a95053d31a24da31ccce0c694deca3efdd
beautyhasnoshape <beautyhasnoshape@gmail.com>
2020-12-02 01:43:29 +0100

    performance improved

--- 01.py
+++ 01.py
@@ -1,12 +1,17 @@
 import itertools
 
 with open("01.txt") as f:
-    numbers = [int(a) for a in f.readlines()]
+    numbers = set([int(a) for a in f.readlines()])
 
-    for p in itertools.combinations(numbers, 2):
-        if sum(p) == 2020:
-            print(p[0]*p[1])
+    for p in numbers:
+        if 2020-p in numbers:
+            print(p*(2020-p))
+            assert 55776 == p*(2020-p)
+            break
 
-    for p in itertools.combinations(numbers, 3):
-        if sum(p) == 2020:
-            print(p[0]*p[1]*p[2])
+    for (a, b) in itertools.combinations(numbers, 2):
+        if 2020-a-b in numbers:
+            print(a*b*(2020-a-b))
+            assert 223162626 == a*b*(2020-a-b)
+            break
+
